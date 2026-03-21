c239ddc67d8118709bb8b1ed5e346679114ac5b3
wyccsc <wyc890619@gmail.com>
2026-03-21 15:04:30 +0800

    frontend-service

--- image/k8s.sh
+++ image/k8s.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker pull wyccsc/k8s_api_service:1.0.7
-docker pull wyccsc/k8s_frontend_service:1.0.7
+docker pull wyccsc/k8s_frontend_service:1.0.8
 docker pull wyccsc/k8s_model_service:1.0.0
